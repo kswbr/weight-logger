dfdefec85750f9c20c3276081fd0c2df0ec599eb
kashiwabara <m_kashiwabara@bbmedia.co.jp>
2018-11-27 13:27:53 +0900

    element-uiを導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  build: {
+    vendor: ['element-ui']
+  },
+  plugins: [
+    '~plugins/element-ui'
+  ],
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
   /*
   ** Headers of the page
   */
