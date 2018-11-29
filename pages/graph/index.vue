7fc576c47e477a8f134a2550390db36079fdbf5d
kashiwabara <m_kashiwabara@bbmedia.co.jp>
2018-11-29 14:05:00 +0900

    chart作り中

--- pages/graph/index.vue
+++ pages/graph/index.vue
@@ -1,19 +1,46 @@
-<template>
   <div class="contents">
-    <buttons />
+    <el-slider v-model="data1" :min="0" :max="100" />
+    <el-slider v-model="data2" :min="0" :max="100" />
   </div>
 </template>
 
 <script>
+import VueCharts from 'vue-chartjs'
+import {Line, mixins} from 'vue-chartjs'
 import Buttons from '~/components/Buttons.vue'
 
 export default {
   components: {
     Buttons
+  },
+  mixins: [Line, mixins.reactiveData],
+  data: function() {
+    return {
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                min: 0,
+                max: 200,
+              }
+            },
+          ]
+        },
+      },
+    }
+  },
+  mounted: function() {
+    this.chartData = {
+      datasets: [
+        {
+          label: 'weight',
+          data: [100,200,100,120],
+        },
+      ],
+    }
   }
 }
 </script>
 
-<style scoped>
-</style>
 
